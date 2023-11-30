83c488e4048a9e18e84b1c76f3e048a514e0873c
nana <goldbarrr5@gmail.com>
2023-11-30 11:06:41 +0900

    토글 방식 변경, 디자인 수정

--- 01/js/script.js
+++ 01/js/script.js
@@ -7,6 +7,9 @@ $(function () {
   // 질문을 클릭했을 때
   $faqQ.on('click', function () {
     // 답변을 보여주자(slide down)
-    $(this).find($faqA).stop().slideDown(duration);
+    $(this).find($faqA).stop().slideToggle(duration);
+
+    // 클릭된 li에게 on 클래스 부여
+    $(this).toggleClass('on');
   });
 });
